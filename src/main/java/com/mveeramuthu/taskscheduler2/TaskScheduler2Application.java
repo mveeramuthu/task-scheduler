3ef651ac4948f8ab3ca7e2e53265180beac30bca
Mohan Veeramuthu <mohan.veeramuthu@ask.com>
2020-10-30 16:13:15 -0700

    initial commit

---
+++ src/main/java/com/mveeramuthu/taskscheduler2/TaskScheduler2Application.java
@@ -0,0 +1,13 @@
+package com.mveeramuthu.taskscheduler2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskScheduler2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskScheduler2Application.class, args);
+    }
+
+}
