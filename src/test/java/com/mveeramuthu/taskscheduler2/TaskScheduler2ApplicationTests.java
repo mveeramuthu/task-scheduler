d307d815a86378c8172b1df0e005da693456b83c
Mohan Veeramuthu <mohan.veeramuthu@ask.com>
2020-10-30 16:13:15 -0700

    initial commit

---
+++ src/test/java/com/mveeramuthu/taskscheduler2/TaskScheduler2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mveeramuthu.taskscheduler2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskScheduler2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
