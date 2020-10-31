c47aa53b5b8aad31e5a63260308177cb3e4074e3
Mohan Veeramuthu <mveeramuthu10@gmail.com>
2020-10-30 19:16:23 -0700

    hello world basic

--- src/main/java/com/mveeramuthu/taskscheduler/TaskSchedulerApplication.java
+++ src/main/java/com/mveeramuthu/taskscheduler/TaskSchedulerApplication.java
@@ -2,12 +2,19 @@ package com.mveeramuthu.taskscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TaskSchedulerApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World - v3!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TaskSchedulerApplication.class, args);
     }
-
 }
